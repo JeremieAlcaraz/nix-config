9e114c5f754603a84ae34af8cf185a8eb1951ae5
Jeremie Alcaraz <hello@jeremiealcaraz.com>
2025-12-24 18:34:58 +0100

    Tweak wezterm keymap and split bindings

--- modules/dotfiles/wezterm/keys/panes.lua
+++ modules/dotfiles/wezterm/keys/panes.lua
@@ -10,20 +10,34 @@ local M = {}
 function M.get_keys()
 	return {
 		{
+			key = "h",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "j",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "k",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "l",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Right"),
+		},
+		{
 			key = "V",
 			mods = "CMD|ALT|CTRL|SHIFT",
-			action = act.Multiple({
-				act.EmitEvent("debug-split-vertical"),
-				act.SplitVertical({ domain = "CurrentPaneDomain" }),
-			}),
+			action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
 		},
 		{
 			key = "H",
 			mods = "CMD|ALT|CTRL|SHIFT",
-			action = act.Multiple({
-				act.EmitEvent("debug-split-horizontal"),
-				act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-			}),
+			action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 		},
 	}
 end
