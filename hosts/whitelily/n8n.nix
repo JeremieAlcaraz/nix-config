2ee697eabbc7fcf63a2c49d248a59a32c9d64c55
JeremieAlcaraz <190272580+JeremieAlcaraz@users.noreply.github.com>
2026-03-31 04:32:11 +0000

    ⬆️ Update n8n to next@sha256:5ff0ad333090d1901005cd764399b1be2c2a29b816704107f08c340d0d223043

--- hosts/whitelily/n8n.nix
+++ hosts/whitelily/n8n.nix
@@ -224,7 +224,7 @@ EOF
     backend = "podman";
     containers.n8n = {
 <<<<<<< HEAD
-      image = "docker.io/n8nio/n8n:next@sha256:565e7df2b15c18f469f8b2d43e0349eaf6a500a0066e5adcce98e441cebfe6f8";  # Tag next pour les dernières betas
+      image = "docker.io/n8nio/n8n:next@sha256:5ff0ad333090d1901005cd764399b1be2c2a29b816704107f08c340d0d223043";  # Tag next pour les dernières betas
       autoStart = true;
       # Pas de ports mapping avec --network host (le conteneur utilise directement les ports de l'hôte)
 
