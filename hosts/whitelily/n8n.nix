ddab5a43bb79ba42e15140db1c60b67a28748937
JeremieAlcaraz <190272580+JeremieAlcaraz@users.noreply.github.com>
2026-05-13 05:43:26 +0000

    ⬆️ Update n8n to next@sha256:4b8acf6a9e21ee4234850557b3c1c46b940f4025a8ddb0f697f858b788103ebe

--- hosts/whitelily/n8n.nix
+++ hosts/whitelily/n8n.nix
@@ -223,7 +223,7 @@ EOF
   virtualisation.oci-containers = {
     backend = "podman";
     containers.n8n = {
-      image = "docker.io/n8nio/n8n:next@sha256:565e7df2b15c18f469f8b2d43e0349eaf6a500a0066e5adcce98e441cebfe6f8";  # Tag next pour les dernières betas
+      image = "docker.io/n8nio/n8n:next@sha256:4b8acf6a9e21ee4234850557b3c1c46b940f4025a8ddb0f697f858b788103ebe";  # Tag next pour les dernières betas
       autoStart = true;
       # Pas de ports mapping avec --network host (le conteneur utilise directement les ports de l'hôte)
 
